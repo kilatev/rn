042ad7e228eb2ab262efb46ba524a76504f84b96
vetalik <vetalik@gmail.com>
2015-12-01 08:31:13 +0000

    added dependencies.

--- riganinja/riganinja/settings.py
+++ riganinja/riganinja/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'feedreader',
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '.'
