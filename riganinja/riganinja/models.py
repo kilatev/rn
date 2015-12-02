21a6818fcbd632304569403d52864af19cafe0df
vetalik <vetalik@gmail.com>
2015-12-02 08:49:47 +0000

    adding more requirements. Init models

---
+++ riganinja/riganinja/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Channel(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    link = models.CharField(max_length=2083)
+    lastBuildDate = models.DateTimeField()
+    generator = models.CharField(max_length=255)
+    language = models.CharField(max_length=6)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=2083)
+    guid = models.CharField(max_length=36)
+    description = models.TextField()
+    author = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    pubDate = models.DateTimeField()
+    channel = models.ForeignKey(Channel)
